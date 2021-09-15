a75abfea33a49df0f352a0f626c845f6dd24dc03
rootkit7628 <arleme.dev7@gmail.com>
2021-09-15 12:58:50 +0300

    API insert content finished

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 import os
 import jwt
+import time
 import mysql.connector
 from datetime import datetime, timedelta
+from werkzeug.utils import secure_filename
 
 from flask import Flask , request, jsonify, render_template, send_from_directory
 from flask_cors import CORS
@@ -15,12 +17,18 @@ CORS(app)
 db = mysql.connector.connect(**database())
 cursor = db.cursor()
 
+path = os.getcwd()
+if not os.path.isdir('data'):
+    os.makedirs('data\contents')
+
+UPLOAD_FOLDER = os.path.join(path, 'data\contents')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Utils function
 def encode_auth_token(user_id):
     payload = {
         'exp': datetime.utcnow() + timedelta(days=7),
-        'sub': user_id 
+        'sub': user_id
     }
     return jwt.encode(
         payload,
@@ -30,7 +38,8 @@ def encode_auth_token(user_id):
 
 def verifToken(token):
     try:
-        return jwt.decode(token, os.environ.get('TOKEN_KEY'), algorithms='HS256', options={"verify_signature": True})
+        return jwt.decode(token, 'TOKEN_KEY', 
+            algorithms='HS256', options={"verify_signature": True})
     except Exception as err:
         print(err)
         return {"sub":0}
@@ -66,7 +75,58 @@ def login():
             'admin': str(user_data[1])
         }), 200
     else:
-        return jsonify({'status': 'Adresse email ou mot de passe incorrect',}), 403
+        return jsonify({'status': 'incorrect_pass',}), 403
+
+
+@app.route("/api/v1/insert_content/", methods=['POST'])
+def insert_content():
+    """
+        DESC : Fonction permettant enregistrer un nouveau contenu'
+    """
+    data = request.get_json()
+    data = request.form
+
+    print(data)
+
+    db = mysql.connector.connect(**database())
+    cursor = db.cursor()
+
+    token = data.get("token")
+    user_id = data.get("user_id")
+
+    if verifToken(token).get('sub') != int(user_id):
+        return {"status" : "Erreur Token"}, 403
+
+    content = {
+        'title' : data.get("title"),
+        'description' : data.get("description"),
+        'text' : data.get("text")
+    }
+
+    # Upload files if exists
+    uploaded_files = request.files.getlist("file[]")
+    file_list = ''
+
+    if uploaded_files:
+        for file in uploaded_files:
+            filename = str(time.time()) + '_' + secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            file_list = filename if ';' not in file_list else file_list + ';' + filename
+
+
+    db = mysql.connector.connect(**database())
+    cursor = db.cursor()
+
+    cursor.execute("""
+            INSERT INTO Contenu(title, description, text, user_id, files) VALUES(%s, %s, %s, %s, %s)
+        """,(content['title'], content['description'], content['text'], user_id, file_list)
+    )
+
+    db.commit()
+    db.close()
+
+    return jsonify({'status': 'content_created'}), 201
 
 if __name__=="__main__":
 	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 4444)), debug=True)
