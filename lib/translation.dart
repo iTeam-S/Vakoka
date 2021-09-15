d00c5b4a3dad63ea48d9a52c4ba0fbb4ad62e278
Gaetan Jonathan <gaetan.s118@gmail.com>
2021-09-15 15:33:35 +0300

    Ajout fichier de traduction

---
+++ lib/translation.dart
@@ -0,0 +1,18 @@
+var translate = {
+  'bienvenue': {'fr': 'Bienvenue', 'en': 'Welcome', 'mg': 'Tongasoa'},
+  'incorrect_pass': {
+    'fr': 'Mot de passe ou Nom d\'utilisateur incorrecte',
+    'en': 'Password or Username invalid',
+    'mg': 'Misy diso ny anarana na soratra miafina nampidirinao',
+  },
+  'email_ou_numero_telephone': {
+    'fr': 'Email ou Numero de téléphone',
+    'en': 'Mail or Phone Number',
+    'mg': 'Mailaka na Laharan\'ny finday',
+  },
+  'se_connecter': {
+    'fr': 'connecter',
+    'en': 'connect',
+    'mg': 'Hiditra',
+  }
+};
